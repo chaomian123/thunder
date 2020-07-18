39e0301fef7c56037249b99d5b4a45edbc042527
zip <892284133@qq.com>
2020-07-18 09:30:41 +0800

    update

--- AppDelegate.m
+++ AppDelegate.m
@@ -19,15 +19,24 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
-    CGImageRef *img = CGWindowListCreateImage(CGRectInfinite, kCGWindowListOptionOnScreenOnly, kCGNullWindowID, kCGWindowImageDefault);
+    CGImageRef img = CGWindowListCreateImage(CGRectMake(10, 50, 100, 100),
+                                              kCGWindowListOptionOnScreenOnly,
+                                              kCGNullWindowID,
+                                              kCGWindowImageDefault);
+    
+    //拿到原始的像素数据
+    CGDataProviderRef provider = CGImageGetDataProvider(img);
+    CFDataRef imageData = CGDataProviderCopyData(provider);
+    uint8_t *bytes = (uint8_t *)CFDataGetBytePtr(imageData);
+    
     NSImage *image = [[NSImage alloc] initWithCGImage:img size:NSZeroSize];
     NSImageView *imgView = [NSImageView imageViewWithImage:image];
     NSLog(@"%f  %f",
           image.size.width,
           imgView.frame.size.width
           );
-    imgView.frame = CGRectMake(10, 30, 720, 480);
-
+    imgView.frame = CGRectMake(0, 0, 100, 100);
+  
     [self.window.contentView addSubview:imgView];
 }
 
